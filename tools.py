271bc7b5009deba75dec2787726da1fb619c3ff0
Apoorv Kumar Jha <apoorv172900@gmail.com>
2026-07-11 06:39:23 +0530

    Improve docstring for web_search function
    Updated docstring for web_search function to clarify the search scope and reliability of information.

--- tools.py
+++ tools.py
@@ -11,7 +11,7 @@ tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 @tool
 def web_search(query : str) -> str:
-    """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
+    """Search the whole web for recent,credible  and reliable information on a topic . Returns Titles , URLs and snippets."""
     results = tavily.search(query=query,max_results=5)
 
     out = []
